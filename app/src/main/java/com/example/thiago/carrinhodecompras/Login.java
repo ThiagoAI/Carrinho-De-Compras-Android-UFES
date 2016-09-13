e57e01a015f79ba8d5681bf0bf7f8c6586f6858c
ThiagoAI <thiagoandersimhoff@gmail.com>
2016-09-13 15:11:52 -0300

    Tela de login avança para tela principal e para tela de registrar.

--- app/src/main/java/com/example/thiago/carrinhodecompras/Login.java
+++ app/src/main/java/com/example/thiago/carrinhodecompras/Login.java
@@ -1,7 +1,10 @@
 package com.example.thiago.carrinhodecompras;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //Função que verifica login e se correto entra na tela principal
+    public void logar(View view){
+        Intent intent = new Intent(this,TelaPrincipal.class);
+        EditText editLogin = (EditText) findViewById(R.id.editLogin);
+        EditText editSenha = (EditText) findViewById(R.id.editSenha);
+        String login = editLogin.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        /*
+         *   VERIFICAÇÃO AQUI
+         */
+
+        startActivity(intent);
+    }
+
+    //Vai para a tela de registro
+    public void irRegistrar(View view){
+        Intent intent = new Intent(this,Registrar.class);
+        startActivity(intent);
     }
 }
