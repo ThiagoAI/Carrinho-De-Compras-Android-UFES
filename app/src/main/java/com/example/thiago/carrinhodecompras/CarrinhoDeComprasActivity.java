e326ce92cd421b1bce5e254d4a5adc11f722e572
Eu <admin@adminvirtual.com>
2016-09-17 17:50:00 -0300

    Primeiro commit com as tela do carrinho de compras (Tela em Andamento). Outras mudanças: *Adicionado suporte para Java 7; *Adicionado suporte ao RecyclerView; *Novas Strings

---
+++ app/src/main/java/com/example/thiago/carrinhodecompras/CarrinhoDeComprasActivity.java
@@ -0,0 +1,13 @@
+package com.example.thiago.carrinhodecompras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarrinhoDeComprasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrinho_de_compras);
+    }
+}
