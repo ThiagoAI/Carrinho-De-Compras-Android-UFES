dad41950ff766c42004b8ae7bf40285104f39886
Eu <admin@adminvirtual.com>
2016-09-18 20:34:52 -0300

    Terceiro Commit: Telas Prontas

---
+++ app/src/main/java/com/example/thiago/carrinhodecompras/EstoqueCasaActivity.java
@@ -0,0 +1,13 @@
+package com.example.thiago.carrinhodecompras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EstoqueCasaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estoque_casa);
+    }
+}
