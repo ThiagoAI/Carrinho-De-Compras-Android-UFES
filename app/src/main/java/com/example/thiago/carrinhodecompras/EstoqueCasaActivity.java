8ef92a1340a50e0488783968beededb46a20f656
Eu <jdopsjdposjdposjdp@ojpojspojdpojspdoj.com>
2016-10-23 22:02:05 -0200

    Funcionalidades Produto Carrinho - Teste - Parte 10

--- app/src/main/java/com/example/thiago/carrinhodecompras/EstoqueCasaActivity.java
+++ app/src/main/java/com/example/thiago/carrinhodecompras/EstoqueCasaActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EstoqueCasaActivity extends Lifecycle
 {
 
 
     private FloatingActionButton botaoAdicionar;
+    private List<Product> produtosEmEstoque;
+    private EstoqueAdapter adapter;
 
 
     @Override
@@ -18,6 +25,14 @@ public class EstoqueCasaActivity extends Lifecycle
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estoque_casa);
+
+        produtosEmEstoque = new ArrayList<>();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.estoqueRecyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 0 ) );
+        adapter = new EstoqueAdapter(produtosEmEstoque);
+        recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new ItemDivider(this));
 
         botaoAdicionar = (FloatingActionButton) findViewById(R.id.botaoAdicionar);
         botaoAdicionar.setOnClickListener(clickListenerBotaoAdicionar);
@@ -28,8 +43,8 @@ public class EstoqueCasaActivity extends Lifecycle
         @Override
         public void onClick(View view)
         {
-            /*Intent intent = new Intent(this,AdicionarEstoqueActivity.class);
-            startActivity(intent);*/
+            //Intent intent = new Intent(this, AdicionarEstoqueActivity.class ); //Intent(this,AdicionarEstoqueActivity.class);
+            //startActivity(intent);
         }
     };
 }
